ea3caec59399f1455b7b89da9a6c478564229e78
Silas Spaar <sspaar@richmondevents.com>
2026-07-06 20:55:54 +0200

    v75: safe-area fix + Kamera/Galerie Chooser

---
+++ android/app/src/main/java/com/comparadoo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.comparadoo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'comparadoo-v74';
+const CACHE_NAME = 'comparadoo-v75';
 const STATIC_ASSETS = ['/', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
